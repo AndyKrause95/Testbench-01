59ad27c27fcd9b72a1d1f45eb975571faf9af70d
André Krause <andre.krause95@gmail.com>
2023-03-30 22:34:02 +0200

    changed heading

--- dist/index.php
+++ dist/index.php
@@ -4,7 +4,7 @@ echo "<h1>New Heading!</h1>";
 
 echo "This is just a test for GitHub! Hello World!";
 echo "</br>";
-echo "Test-feature";
+echo "This is a Test-feature";
 
 echo "<h1> Environment Variable Testing</h1>";
 
@@ -14,6 +14,7 @@ echo "</br>";
 echo "Username: " . getenv("DB_USER");
 echo "</br>";
 echo "Database: " . getenv("DB_NAME");
+echo "</br>";
 echo "</br>";
 echo "</br>";
 echo "by Andy";
