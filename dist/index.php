3371f2773130edfacc4a0743a605a726638ada50
André Krause <andre.krause95@gmail.com>
2023-03-30 21:38:06 +0200

    - first attemp at using env variables

--- dist/index.php
+++ dist/index.php
@@ -1,5 +1,15 @@
 <?php
 
 echo "This is just a test! Hello World!";
-echo "<br>";
+echo "</br>";
 echo "Test-feature";
+
+echo "<h1> Environment Variable Testing</h1>";
+
+echo "</br>";
+echo "</br>";
+
+echo "Username: " . getenv("DB_USER");
+echo "</br>";
+echo "Database: " . getenv("DB_NAME");
+echo "</br>";
